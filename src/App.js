1adb497cf35220f5ea2bb6b8ced0caa894583759
tmirdin <mirdin.981@gmail.com>
2022-10-27 12:02:54 +0600

    second maket

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import AddProduct from "./Components/Admin/AddProduct/AddProduct";
 import NavBar from "./Components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <AddProduct />
     </div>
   );
 };
--- src/Components/Admin/AddProduct/AddProduct.jsx
+++ src/Components/Admin/AddProduct/AddProduct.jsx
@@ -1,21 +1,51 @@
-import { Box, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import React from "react";
+import "./AddProduct.css";
 
 const AddProduct = () => {
   return (
     <>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "55ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <br />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      </Box>
+      <h2 id="add-title">Добавление товара</h2>
+      <form id="form-add">
+        <TextField
+          className="outlined-basic"
+          label="Категория"
+          variant="outlined"
+        />
+        <TextField
+          className="outlined-basic"
+          label="Название"
+          variant="outlined"
+        />
+        <TextField
+          className="outlined-basic"
+          label="Модель"
+          variant="outlined"
+        />
+        <TextField
+          className="outlined-basic"
+          label="Описание"
+          variant="outlined"
+        />
+        <TextField className="outlined-basic" label="Цвет" variant="outlined" />
+        <TextField className="outlined-basic" label="Цена" variant="outlined" />
+        <TextField
+          className="outlined-basic"
+          label="Фото 1"
+          variant="outlined"
+        />
+        <TextField
+          className="outlined-basic"
+          label="Фото 2"
+          variant="outlined"
+        />
+        <TextField
+          className="outlined-basic"
+          label="Фото 3"
+          variant="outlined"
+        />
+        <Button variant="contained">Добавить</Button>
+      </form>
     </>
   );
 };
