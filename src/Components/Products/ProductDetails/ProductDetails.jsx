f48a7cab6b31ab18d222eadd062d686fc36d601b
tmirdin <mirdin.981@gmail.com>
2022-10-31 14:49:18 +0600

    after

--- src/Components/Products/ProductDetails/ProductDetails.jsx
+++ src/Components/Products/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Paper } from "@mui/material";
+import { Container, Grid, Paper, Typography } from "@mui/material";
 import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -25,51 +25,54 @@ const ProductDetails = () => {
   return (
     <>
       {productDetails ? (
-        <Container>
-          <Grid item xs={6}>
-            <Swiper
-              className="mySwiper2"
-              spaceBetween={10}
-              thumbs={{ swiper: thumbsSwiper }}
-            >
-              <SwiperSlide>
-                <img src={productDetails.img1} alt={productDetails.title} />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src={productDetails.img2} alt={productDetails.title} />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src={productDetails.img3} alt={productDetails.title} />
-              </SwiperSlide>
-            </Swiper>
-            <Swiper
-              onSwiper={setThumbsSwiper}
-              spaceBetween={10}
-              slidesPerView={4}
-              freeMode={true}
-              watchSlidesProgress={true}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <Paper elevation={3}>
-                  <img
-                    width={50}
-                    src={productDetails.img1}
-                    alt={productDetails.title}
-                  />
-                </Paper>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Paper elevation={3}>
+        <Container sx={{ marginTop: "40px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Swiper
+                className="mySwiper2"
+                spaceBetween={10}
+                thumbs={{ swiper: thumbsSwiper }}
+              >
+                <SwiperSlide>
+                  <img src={productDetails.img1} alt={productDetails.title} />
+                </SwiperSlide>
+                <SwiperSlide>
                   <img src={productDetails.img2} alt={productDetails.title} />
-                </Paper>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Paper elevation={3}>
+                </SwiperSlide>
+                <SwiperSlide>
                   <img src={productDetails.img3} alt={productDetails.title} />
-                </Paper>
-              </SwiperSlide>
-            </Swiper>
+                </SwiperSlide>
+              </Swiper>
+              <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <Paper elevation={3}>
+                    <img src={productDetails.img1} alt={productDetails.title} />
+                  </Paper>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Paper elevation={3}>
+                    <img src={productDetails.img2} alt={productDetails.title} />
+                  </Paper>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Paper elevation={3}>
+                    <img src={productDetails.img3} alt={productDetails.title} />
+                  </Paper>
+                </SwiperSlide>
+              </Swiper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper elevation={3} sx={{ padding: "10px" }}>
+                <Typography variant="h4">{productDetails.title}</Typography>
+              </Paper>
+            </Grid>
           </Grid>
         </Container>
       ) : null}
